8d161442de8f3bb17d69ba7504c06364c1421915
Zamy846692 <133600041+Zamy846692@users.noreply.github.com>
2025-05-30 17:06:12 +0100

    Added new bindings to TkinterHv3

--- tkinterweb/bindings.py
+++ tkinterweb/bindings.py
@@ -1861,9 +1861,9 @@ class TkinterHv3(tk.Widget):
 
     def _requestcmd(self, handle):
         uri = self.tk.call(handle, "cget", "-uri")
+        print(uri)
         head_str = " ".join(f"{k} {{{v}}}" for k, v in self.headers.items())
         self.tk.call(handle, "configure", "-header", head_str)
-        print(uri)
         kw = dict(url=uri, insecure=False, headers=(self.tk.call(handle, "cget", "-header"),))
         parsed = self.tk.call("::tkhtml::uri", uri)
         if self.tk.call(parsed, "scheme") == "file": data = download(**kw)
@@ -1878,3 +1878,21 @@ class TkinterHv3(tk.Widget):
 
     def stop(self):
         self.tk.call(self._w, "stop")
+
+    def reset(self):
+        self.tk.call(self._w, "reset")
+
+    def node(self):
+        return self.tk.call(self._w, "node")
+
+    def html(self):
+        return self.tk.call(self._w, "html")
+
+    def title(self):
+        return self.tk.call(self._w, "title")
+
+    def location(self):
+        return self.tk.call(self._w, "location")
+
+    def selected(self):
+        return self.tk.call(self._w, "selected")
